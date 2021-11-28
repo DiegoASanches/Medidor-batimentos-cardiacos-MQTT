af26053a251a6660af5e711fe81967e2c17a9534
Diego Sanches <diegoasancheess@gmail.com>
2021-11-28 14:13:42 -0300

    first commit

---
+++ PulseSensorAmped_Arduino_1.5.0.ino
@@ -0,0 +1,169 @@
+
+/*  Pulse Sensor Amped 1.5    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
+
+  ----------------------  Notes ----------------------  ----------------------
+  This code:
+  1) Blinks an LED to User's Live Heartbeat   PIN 13
+  2) Fades an LED to User's Live HeartBeat    PIN 5
+  3) Determines BPM
+  4) Prints All of the Above to Serial
+
+  Read Me:
+  https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README.md
+  ----------------------       ----------------------  ----------------------
+*/
+
+#define PROCESSING_VISUALIZER 1
+#define SERIAL_PLOTTER  2
+
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <PubSubClient.h>
+
+// Function prototypes
+void subscribeReceive(char* topic, byte* payload, unsigned int length);
+
+const char* server = "broker.emqx.io";
+
+// Set your MAC address and IP address here
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// Ethernet and MQTT related objects
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
+
+const char* mqttUser = "diegoasancheess@gmail.com";
+const char* mqttPassword = "Extensao@01";
+
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 7;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 255;                 // used to fade LED on with PWM on fadePin
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 500;             // int that holds the time interval between beats! Must be seeded!
+volatile boolean Pulse = true;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// SET THE SERIAL OUTPUT TYPE TO YOUR NEEDS
+// PROCESSING_VISUALIZER works with Pulse Sensor Processing Visualizer
+//      https://github.com/WorldFamousElectronics/PulseSensor_Amped_Processing_Visualizer
+// SERIAL_PLOTTER outputs sensor data for viewing with the Arduino Serial Plotter
+//      run the Serial Plotter at 115200 baud: Tools/Serial Plotter or Command+L
+static int outputType = SERIAL_PLOTTER;
+
+
+void setup() {
+  pinMode(blinkPin, OUTPUT);        // pin that will blink to your heartbeat!
+  pinMode(fadePin, OUTPUT);         // pin that will fade to your heartbeat!
+  Serial.begin(9600);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+  // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE,
+  // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+  //   analogReference(EXTERNAL);
+
+
+  // Start the ethernet connection
+  Ethernet.begin(mac);
+
+  // Ethernet takes some time to boot!
+  delay(3000);
+
+  // Set the MQTT server to the server stated above ^
+  mqttClient.setServer(server, 1883);
+
+  // Attempt to connect to the server with the ID "myClientID"
+  if (mqttClient.connect("mqtt_3128"))
+  {
+    Serial.println("Connection has been established, well done");
+
+    // Establish the subscribe event
+    mqttClient.setCallback(subscribeReceive);
+  }
+  else
+  {
+    Serial.println("Looks like the server connection failed...");
+  }
+}
+
+
+//  Where the Magic Happens
+void loop() {
+  //Serial.println(Signal);
+  //serialOutput() ;
+
+  Serial.println(QS);
+
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  mqttClient.loop();
+
+  if (QS == true) {    // A Heartbeat Was Found
+    // BPM and IBI have been Determined
+    // Quantified Self "QS" true when arduino finds a heartbeat
+    fadeRate = 255;         // Makes the LED Fade Effect Happen
+    // Set 'fadeRate' Variable to 255 to fade LED with pulse
+    serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
+    QS = false;                      // reset the Quantified Self flag for next time
+
+    // Attempt to publish a value to the topic "MakerIOTopic"
+    String bpmString = "";
+    if (mqttClient.publish("/nodejs/mqtt", BPM))
+    {
+      Serial.println("Publish message success");
+    }
+    else
+    {
+      Serial.println("Could not send message :(");
+    }
+  }
+
+  ledFadeToBeat();
+  delay(20);    // Makes the LED Fade Effect Happen
+  //  take a break
+}
+
+
+void ledFadeToBeat() {
+  fadeRate -= 15;                         //  set LED fade value
+  fadeRate = constrain(fadeRate, 0, 255); //  keep LED fade value from going into negative numbers!
+  analogWrite(fadePin, fadeRate);         //  fade LED
+}
+
+void subscribeReceive(char* topic, byte* payload, unsigned int length)
+{
+  // Print the topic
+  Serial.print("Topic: ");
+  Serial.println(topic);
+
+  // Print the message
+  Serial.print("Message: ");
+  for (int i = 0; i < length; i ++)
+  {
+    Serial.print(char(payload[i]));
+  }
+
+  // Print a newline
+  Serial.println("");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect("mqtt_21921912", mqttUser, mqttPassword)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
